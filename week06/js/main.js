42ab0a29c6c9474d286e27d9934bcf24c19fb986
Alina Inchaustegui <alinainchaustegui@gmail.com>
2023-03-30 18:07:53 -0400

    alternating case

--- week06/js/main.js
+++ week06/js/main.js
@@ -195,3 +195,22 @@ function countSheeps(arrayOfSheep) {
  function countSheeps(arrayOfSheeps) {
   return arrayOfSheeps.filter(element => element === true).length;
 }
+//#15 altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+String.prototype.toAlternatingCase = function () {
+  let letters = this.split('') //.this accesses string
+  let letters2 = letters.map(swap)
+  console.log(letters)
+  let letters3 = letters2.join('')
+  console.log(letters3)
+  return letters3
+  }
+  
+  function swap(x){
+    if(x === x.toLowerCase()){
+      return x.toUpperCase()
+    } else if(x === x.toUpperCase()){
+      return x.toLowerCase()
+    } else {
+      return x
+    }
+  }
