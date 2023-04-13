57ba3a4ed041385f1c134f49d0c34d5f3bd6a330
Alina Inchaustegui <alinainchaustegui@gmail.com>
2023-04-12 21:41:35 -0400

    wed codewars

--- week06/js/main.js
+++ week06/js/main.js
@@ -327,3 +327,17 @@ function sum (numbers) {
 
 //with arrow function
 const sum = numbers => numbers.reduce((a,n) => a + n, 0)
+
+//#30 Calculating average 8kyu
+function findAverage(array) {
+  if(array.length === 0){
+    return 0
+  } else{
+    let sum = array.reduce((a,n) => a + n, 0)
+    let valueFinal = sum / array.length
+    return valueFinal
+  }
+}
+
+//add all with reduce method
+// divide by the total amount of numbers (.length)
