cc57ee15e7377702df9cd8583b2de4f253b1676e
Alina Inchaustegui <alinainchaustegui@gmail.com>
2023-04-17 21:17:25 -0400

    string repeat

--- week06/js/main.js
+++ week06/js/main.js
@@ -385,5 +385,8 @@ function feast(beast, dish){
 
 const feast = (beast, dish) => (beast[0] === dish[0] && beast[beast.length-1] === dish[dish.length-1])
 
+//#36 String repeat
+const repeatStr = (n,s) => s.repeat(n)
+
 
  
