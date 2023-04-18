59aceebccc9d620be93ba08a32475d5631596325
Alina Inchaustegui <alinainchaustegui@gmail.com>
2023-04-17 21:07:44 -0400

    feast of many beasts

--- week06/js/main.js
+++ week06/js/main.js
@@ -373,3 +373,13 @@ function removeChar(str){
 }
 
 const removeChar = str => str.slice(1, -1)
+
+//#35 The Feast of Many Beast (8kyu)
+function feast(beast, dish){
+  if(beast.charAt(0) === dish.charAt(0) && beast.charAt(beast.length-1) === dish.charAt(dish.length-1)){
+      return true
+  } else {
+      return false
+  }
+}
+ 
