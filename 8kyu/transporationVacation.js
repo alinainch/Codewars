89379a8405012d1f6d5912275532cbf378046ce8
Alina Inchaustegui <alinainchaustegui@gmail.com>
2023-04-22 12:13:16 -0400

    transportation vacation

---
+++ 8kyu/transporationVacation.js
@@ -0,0 +1,12 @@
+function rentalCarCost(d) {
+  cost = d * 40
+  if(d >= 7){
+    cost -= 50
+    return cost
+  } else if(d >= 3){
+    cost -= 20
+    return cost
+  } else {
+  return cost 
+  }
+}
