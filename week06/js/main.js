b06890eaff7f009ed7331679d5eb4b166ce4d7c7
Alina Inchaustegui <alinainchaustegui@gmail.com>
2023-04-24 14:30:05 -0400

    Hello World - Without Strings 7kyu

--- week06/js/main.js
+++ week06/js/main.js
@@ -419,3 +419,8 @@ let greet = function(name) {
   let fixName = name[0].toUpperCase() + name.substring(1).toLowerCase()
   return `Hello ${fixName}!`
 }
+
+//Hello World - Without Strings 7kyu
+const helloWorld = () => {
+  return String.fromCharCode(72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33) 
+ }; 
