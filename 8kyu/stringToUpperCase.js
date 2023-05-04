8dcec1a86430f05f6ae7057610f46129cb0fb760
Alina Inchaustegui <alinainchaustegui@gmail.com>
2023-05-04 17:45:11 -0400

    to uppercase

---
+++ 8kyu/stringToUpperCase.js
@@ -0,0 +1,11 @@
+String.prototype.isUpperCase = function() {
+  return this.toString() === this.toUpperCase()
+}
+ 
+//   let convert = this.toUpperCase()
+//   if(convert === this.toString()){
+//    return true
+//  } else {
+//    return false
+//  }
+//this is a string object not a string 
