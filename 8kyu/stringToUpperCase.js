4be90d24f8c9ab0189882ab4dab375b6b8f00dc2
Alina Inchaustegui <alinainchaustegui@gmail.com>
2023-05-04 18:13:32 -0400

    multiples of 3 or 5

--- 8kyu/stringToUpperCase.js
+++ 8kyu/stringToUpperCase.js
@@ -1,11 +1,4 @@
-String.prototype.isUpperCase = function() {
+String.prototype.isUpperCase = function () {
   return this.toString() === this.toUpperCase()
 }
- 
-//   let convert = this.toUpperCase()
-//   if(convert === this.toString()){
-//    return true
-//  } else {
-//    return false
-//  }
-//this is a string object not a string 
+
