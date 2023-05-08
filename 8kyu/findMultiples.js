ef6feaafdbd5b491fd07a85798e054735b53b647
Alina Inchaustegui <alinainchaustegui@gmail.com>
2023-05-08 10:59:15 -0400

    different solution

--- 8kyu/findMultiples.js
+++ 8kyu/findMultiples.js
@@ -10,3 +10,13 @@ function findMultiples(integer, limit) {
 
 //find multiples so 25/5 = 5 that is the new limit and would be the max amount of time
 //make a new array to push into
+
+function findMultiples(integer, limit) {
+  let arr = []
+  for(let i = integer; i <= limit; i++){
+    if(i % integer === 0){
+      arr.push(i)
+    }
+  }
+  return arr
+}
