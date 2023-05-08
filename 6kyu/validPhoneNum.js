15ac13b076d8603816a034789285b93a62241756
Alina Inchaustegui <alinainchaustegui@gmail.com>
2023-05-08 12:12:34 -0400

    valid phone number

---
+++ 6kyu/validPhoneNum.js
@@ -0,0 +1,7 @@
+function validPhoneNumber(phoneNumber){
+  return /^\(\d{3}\) \d{3}\-\d{4}$/.test(phoneNumber)
+ }
+ 
+ //console.log(phoneNumber)
+ //worked with Julia, Jada, Ashley
+ //test will test the parameter set up and returns a true/false 
