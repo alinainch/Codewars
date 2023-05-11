a652a642d8e1480beb734d26f20e6c4b6c1c7327
Alina Inchaustegui <alinainchaustegui@gmail.com>
2023-05-11 12:01:05 -0400

    make six toast

---
+++ 8kyu/sixToast.js
@@ -0,0 +1,19 @@
+function sixToast(num){
+  if(num >= 6){
+    return num - 6
+  } else if(num < 6){
+    return 6 - num 
+  }
+}
+
+
+// function sixToast(num) {
+//   return Math.abs(num - 6)
+// }
+
+
+//has to equal 6
+// num - 6 if greater than 6
+// if less than 6 return 6 - num 
+
+
