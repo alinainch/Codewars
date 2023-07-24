aabd8b8ef4f29c3549fb4c7181e4864d8e28651c
Alina Inchaustegui <alinainchaustegui@gmail.com>
2023-07-24 16:50:03 -0400

    sort by refactored with the numerical result of fn

--- Leetcode/sortBy.js
+++ Leetcode/sortBy.js
@@ -11,3 +11,20 @@
   }
   return sortedArr
 };
+
+
+//
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+ var sortBy = function(arr, fn) {
+  //sort by the numerical result of the function
+  arr.sort((a, b) => fn(a) - fn(b))
+  let sortedArr = []
+  for(let i = 0; i < arr.length; i++){
+      sortedArr.push(arr[i])
+  }
+  return sortedArr
+};
