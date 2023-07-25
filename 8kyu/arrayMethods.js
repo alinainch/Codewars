acb5c71ec270a3a997147055f7ff7420ae705009
Alina Inchaustegui <alinainchaustegui@gmail.com>
2023-07-25 09:26:30 -0400

    filter and forEach

---
+++ 8kyu/arrayMethods.js
@@ -0,0 +1,20 @@
+// Output: 1, 2, 3
+let arr = [1, 2, 3]
+for(let i = 0; i < arr.length; i++){
+  console.log(arr[i])
+}
+
+
+//forEach
+arr.forEach(e => console.log(e))
+
+//filter
+const canDrink = ages.filter(age => age >= 21)
+
+const retail = companies.filter(e => company.category === 'retail')
+
+const eightiesCompanies = companies.filter(e => company.start >= 1980 && company.start < 1990)
+
+//map (creates new array)
+
+
