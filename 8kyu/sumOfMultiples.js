0934d4fdb7355c30033523efead36f911338e08e
Alina Inchaustegui <alinainchaustegui@gmail.com>
2023-07-25 12:55:34 -0400

    Sum of Multiples

--- 8kyu/sumOfMultiples.js
+++ 8kyu/sumOfMultiples.js
@@ -9,3 +9,8 @@ function sumMul(n,m){
   }
   return sum;
 }
+
+// the count (i) starts at the number n 
+// run while i is < m
+//In each iteration, i is incremented by n, which means we are progressing through multiples of n
+// sumMul(3, 13), the loop will start with i = 3, then i will be incremented to 6, 9, and 12. Since the loop condition is i < 13, the loop will stop before i becomes 13, and the function will return 3 + 6 + 9 + 12 = 30, which is the sum of all multiples of 3 below 13.
