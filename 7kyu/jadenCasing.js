589ab6a1a468b3d7daa4985562f11544ae0c73db
Alina Inchaustegui <alinainchaustegui@gmail.com>
2023-07-24 22:53:57 -0400

    Remove Duplicates 8kyu

--- 7kyu/jadenCasing.js
+++ 7kyu/jadenCasing.js
@@ -1,3 +1,8 @@
 String.prototype.toJadenCase = function () {
   return this.split(' ').map(a => a[0].toUpperCase() + a.slice(1)).join(' ')
 }
+
+//take the sentence and make it into an array
+//map every word in the sentence and make to uppercase
+//add the other part of the word
+//join the sentence together 
---
+++ 8kyu/removeDuplicates.js
@@ -0,0 +1,7 @@
+function distinct(a) {
+  return [... new Set(a)]
+}
+
+//Set object is a built-in JavaScript data structure that allows you to store unique values of any type. Automatically removes duplicate elements
+//Set is created by passing the array a as an argument to the Set constructor
+//The spread operator (...) is used to convert the Set back into an array.
