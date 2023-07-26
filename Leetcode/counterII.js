d8d953427317137b3c623df7843922766e2fae6b
Alina Inchaustegui <alinainchaustegui@gmail.com>
2023-07-26 14:51:15 -0400

    Counter 2

---
+++ Leetcode/counterII.js
@@ -0,0 +1,8 @@
+var createCounter = function(init) {
+  let currentValue = init
+  return {
+      increment: () => ++currentValue,
+      decrement: () => --currentValue,
+      reset: () => currentValue = init
+  }
+}
