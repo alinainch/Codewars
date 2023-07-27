f2e9fd42692abd0f8e749b06482e4266e9d8cd73
Alina Inchaustegui <alinainchaustegui@gmail.com>
2023-07-27 11:28:10 -0400

    Contains Duplicates

---
+++ Leetcode/containsDuplicates.js
@@ -0,0 +1,10 @@
+var containsDuplicate = function(nums) {
+  nums.sort()
+  for (let i = 0; i < nums.length - 1; i++) {
+    if(nums[i] === nums[i + 1]){
+      return true
+    } else {
+      return false 
+    }
+  }
+};
