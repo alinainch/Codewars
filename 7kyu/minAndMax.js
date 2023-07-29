669fe155e5bcb24d21004923952d9c49fd156687
Alina Inchaustegui <alinainchaustegui@gmail.com>
2023-07-29 10:02:03 -0400

    Find Min and Max

--- 7kyu/minAndMax.js
+++ 7kyu/minAndMax.js
@@ -2,3 +2,7 @@ function getMinMax(arr){
   let sortedArr = arr.sort((a,b) => a - b)
   return [sortedArr[0], sortedArr[sortedArr.length - 1]]
 };
+
+function getMinMax(arr){
+  return [Math.min(...arr),Math.max(...arr)];
+}
