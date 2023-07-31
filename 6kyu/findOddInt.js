1d2284fe788c3e855f310ac79cfa4a6466616bda
Alina Inchaustegui <alinainchaustegui@gmail.com>
2023-07-31 16:54:05 -0400

    Find the Odd Int

---
+++ 6kyu/findOddInt.js
@@ -0,0 +1,17 @@
+function findOdd(A) {
+  let count = 0;
+  let arr = A.sort((a, b) => a - b);
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i] == arr[j]) {
+        count++;
+      }
+    }
+    if (count % 2 !== 0) {
+      return arr[i];
+    }
+  }
+}
+
+//given array of integers
+//return the number in array that appears odd number of times
