d5271b9f5fbb5ddc41a3e5bfd07ef1e32afac1df
Alina Inchaustegui <alinainchaustegui@gmail.com>
2023-08-06 23:26:49 -0400

    Find the Divisors

---
+++ 7kyu/findTheDivisors.js
@@ -0,0 +1,9 @@
+function divisors(integer) {
+  let array = []
+  for (let i = 2; i < integer; i++) {
+      if (integer % i === 0) {
+          array.push(i)
+      }
+  }
+  return array.length === 0 ? `${integer} is prime` : array
+};
